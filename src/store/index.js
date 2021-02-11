a0d6279b302dedabfec3b9260c46fee71f1dd1b8
Alex <alexmogwaiii@gmail.com>
2021-02-11 13:01:28 +0200

    deleted redux debagger

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,6 @@ const store = createStore(rootReducer, compose(
   applyMiddleware(
     thunk,
   ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ));
 /* eslint-enable */
 
